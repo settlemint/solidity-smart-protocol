9f02e576319812b781b3a4300bf5b0b739336a27
Daan Poron <daan@poron.be>
2025-04-16 17:07:56 +0200

    start adding logic to add claims

--- test/SMART.t.sol
+++ test/SMART.t.sol
@@ -10,19 +10,26 @@ import { SMARTTrustedIssuersRegistry } from "../contracts/SMART/SMARTTrustedIssu
 import { SMARTIdentityRegistry } from "../contracts/SMART/SMARTIdentityRegistry.sol";
 import { SMARTCompliance } from "../contracts/SMART/SMARTCompliance.sol";
 import { SMARTIdentityFactory } from "../contracts/SMART/SMARTIdentityFactory.sol";
+import { IClaimIssuer } from "../contracts/onchainid/interface/IClaimIssuer.sol";
 
 contract SMARTTest is Test {
     address public platformAdmin = makeAddr("Platform Admin");
     address public client1 = makeAddr("Client 1");
 
+    uint256 private issuer1PrivateKey = 0x12345;
+    address public issuer1 = vm.addr(issuer1PrivateKey);
+
+    uint256 public constant CLAIM_TOPIC_KYC = 1;
+    uint256 public constant CLAIM_TOPIC_AML = 2;
+    uint256 public constant ECDSA_TYPE = 1; // Scheme for ECDSA signatures (ERC735)
+
     SMARTIdentityRegistryStorage identityRegistryStorage;
     SMARTTrustedIssuersRegistry trustedIssuersRegistry;
     SMARTIdentityRegistry identityRegistry;
     SMARTCompliance compliance;
+    SMARTIdentityFactory identityFactory;
 
     MySMARTTokenFactory factory;
-
-    SMARTIdentityFactory identityFactory;
 
     function setUp() public {
         vm.startPrank(platformAdmin);
@@ -42,20 +49,119 @@ contract SMARTTest is Test {
         vm.stopPrank();
     }
 
-    function createClientIdentity(address clientWalletAddress_, uint16 countryCode_) public {
+    function _createIdentity(address walletAddress_) public returns (address) {
         vm.startPrank(platformAdmin);
 
+        address identity = identityFactory.createIdentity(walletAddress_, new bytes32[](0));
+
+        vm.stopPrank();
+
+        return identity;
+    }
+
+    function _createClientIdentity(address clientWalletAddress_, uint16 countryCode_) public returns (address) {
         // Create the identity using the factory
-        address identity = identityFactory.createIdentity(clientWalletAddress_, new bytes32[](0));
+        address identity = _createIdentity(clientWalletAddress_);
+
+        vm.startPrank(platformAdmin);
 
         // Register the identity in the registry
         identityRegistry.registerIdentity(clientWalletAddress_, IIdentity(identity), countryCode_);
 
         vm.stopPrank();
+
+        return identity;
+    }
+
+    function _createIssuerIdentity(
+        address issuerWalletAddress_,
+        uint256[] memory claimTopics
+    )
+        public
+        returns (address)
+    {
+        // Create the identity using the factory
+        address issuerIdentityAddr = _createIdentity(issuerWalletAddress_);
+
+        vm.startPrank(platformAdmin);
+        // IMPORTANT: Cast the *identity address* to IClaimIssuer for the registry
+        trustedIssuersRegistry.addTrustedIssuer(IClaimIssuer(issuerIdentityAddr), claimTopics);
+
+        vm.stopPrank();
+
+        return issuerIdentityAddr; // Return the created identity address
+    }
+
+    /**
+     * @notice Creates the claim data, hash, and signature for an ERC735 claim.
+     * @param issuerPrivateKey_ The private key of the issuer to sign the claim.
+     * @param clientIdentityAddr The address of the client's identity contract.
+     * @param claimTopic The topic of the claim.
+     * @param claimDataString The string data of the claim.
+     * @return data The ABI encoded claim data.
+     * @return signature The packed ECDSA signature (r, s, v).
+     */
+    function _createClaimSignature(
+        uint256 issuerPrivateKey_,
+        address clientIdentityAddr,
+        uint256 claimTopic,
+        string memory claimDataString
+    )
+        internal
+        pure
+        returns (bytes memory data, bytes memory signature)
+    {
+        data = abi.encode(claimDataString);
+        bytes32 dataHash = keccak256(abi.encode(clientIdentityAddr, claimTopic, data));
+        bytes32 prefixedHash = keccak256(abi.encodePacked("\\x19Ethereum Signed Message:\\n32", dataHash));
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(issuerPrivateKey_, prefixedHash);
+        signature = abi.encodePacked(r, s, v);
+
+        return (data, signature);
+    }
+
+    function _issueClaim(
+        uint256 issuerPrivateKey_,
+        address issuerIdentityAddr_, // Changed parameter name for clarity
+        address clientWalletAddress_,
+        uint256 claimTopic,
+        string memory claimData
+    )
+        public
+    {
+        // 1. Get client's identity contract
+        IIdentity clientIdentity = identityRegistry.identity(clientWalletAddress_);
+        address clientIdentityAddr = address(clientIdentity);
+        require(clientIdentityAddr != address(0), "Client identity not found");
+
+        // 2. Create signature
+        (bytes memory data, bytes memory signature) =
+            _createClaimSignature(issuerPrivateKey_, clientIdentityAddr, claimTopic, claimData);
+
+        // 3. Client adds the claim to their identity
+        vm.startPrank(clientWalletAddress_);
+        // Pass the issuer's *identity contract address* as the issuer
+        clientIdentity.addClaim(claimTopic, ECDSA_TYPE, issuerIdentityAddr_, signature, data, "");
+        vm.stopPrank();
     }
 
     function test_Mint() public {
         // Create the client identity
-        createClientIdentity(client1, 56);
+        _createClientIdentity(client1, 56);
+
+        // Create the issuer identity
+        uint256[] memory claimTopics = new uint256[](2);
+        claimTopics[0] = CLAIM_TOPIC_KYC;
+        claimTopics[1] = CLAIM_TOPIC_AML;
+        // Store the issuer's identity contract address
+        address issuer1Identity = _createIssuerIdentity(issuer1, claimTopics);
+
+        // Issue claims from issuer1 to client1
+        // Pass the issuer's identity address, not wallet address
+        _issueClaim(issuer1PrivateKey, issuer1Identity, client1, CLAIM_TOPIC_KYC, "Verified KYC by Issuer 1");
+        _issueClaim(issuer1PrivateKey, issuer1Identity, client1, CLAIM_TOPIC_AML, "Verified AML by Issuer 1");
+
+        // TODO: Add assertions to verify claims exist on client1's identity
     }
 }
