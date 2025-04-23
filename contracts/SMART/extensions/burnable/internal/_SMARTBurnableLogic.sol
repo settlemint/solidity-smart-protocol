ab28c50ab03e1d7a7fcc45c0656b4345b3056571
Daan Poron <daan@poron.be>
2025-04-23 17:57:04 +0200

    cleanup

--- contracts/SMART/extensions/burnable/internal/_SMARTBurnableLogic.sol
+++ contracts/SMART/extensions/burnable/internal/_SMARTBurnableLogic.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: CC0-1.0
 pragma solidity ^0.8.27;
 
-import { LengthMismatch } from "../../common/CommonErrors.sol";
+import { LengthMismatch, Unauthorized } from "../../common/CommonErrors.sol";
 import { _SMARTExtension } from "../../common/_SMARTExtension.sol";
+import { _SMARTBurnableAuthorizationHooks } from "./_SMARTBurnableAuthorizationHooks.sol";
 /// @title _SMARTBurnableLogic
 /// @notice Base logic contract for SMARTBurnable functionality.
 /// @dev Contains internal implementations for burning tokens.
 
-abstract contract _SMARTBurnableLogic is _SMARTExtension {
+abstract contract _SMARTBurnableLogic is _SMARTExtension, _SMARTBurnableAuthorizationHooks {
     // --- Abstract Hooks ---
 
     /// @dev Abstract function representing the actual burn operation (e.g., ERC20Burnable._burn).
@@ -20,6 +21,7 @@ abstract contract _SMARTBurnableLogic is _SMARTExtension {
     /// @dev Internal implementation for burning a specific amount of tokens.
     ///      Relies on concrete contract providing `_beforeBurn`, `_burn`, `_afterBurn`.
     function _burnInternal(address userAddress, uint256 amount) internal virtual {
+        if (!_authorizeBurn()) revert Unauthorized();
         _beforeBurn(userAddress, amount);
         // We cannot call _burn directly here, hence _executeBurn.
         _burnable_executeBurn(userAddress, amount);
--- contracts/SMART/extensions/common/interfaces/_AccessControl.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: CC0-1.0
-pragma solidity ^0.8.27;
-
-abstract contract _AccessControl {
-    function hasRole(bytes32 role, address account) public view virtual returns (bool);
-}
