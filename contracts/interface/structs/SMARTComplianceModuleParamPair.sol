ec798f8cdc92489a5306f75b04fba22a0b825cf5
Daan Poron <daan@poron.be>
2025-04-30 16:02:52 +0200

    fix

--- contracts/interface/structs/SMARTComplianceModuleParamPair.sol
+++ contracts/interface/structs/SMARTComplianceModuleParamPair.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.27;
 
 /// @notice Represents a pair of a compliance module address and its associated configuration parameters.
-struct ISMARTComplianceModuleParamPair {
+struct SMARTComplianceModuleParamPair {
     address module;
     bytes params;
 }
