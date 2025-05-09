b7b2aa5db72592691fb556dff0abc1a4fd0a7b2f
Daan Poron <daan@poron.be>
2025-05-09 18:41:06 +0200

    f

--- contracts/extensions/pausable/SMARTPausableEvents.sol
+++ contracts/extensions/pausable/SMARTPausableEvents.sol
@@ -4,8 +4,8 @@ pragma solidity ^0.8.28;
 // -- Events --
 /// @notice Emitted when the contract is paused.
 /// @param sender The address that triggered the pause.
-event Paused(address indexed initiator);
+event Paused(address indexed sender);
 
 /// @notice Emitted when the contract is unpaused.
 /// @param sender The address that triggered the unpause.
-event Unpaused(address indexed initiator);
+event Unpaused(address indexed sender);
