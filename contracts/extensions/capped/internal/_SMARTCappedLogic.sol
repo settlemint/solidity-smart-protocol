7e212616045f469121622a774f08c3b3e6966d34
Roderik van der Veer <roderik@settlemint.com>
2025-05-17 09:13:06 +0200

    chore: standardize solidity pragma

--- contracts/extensions/capped/internal/_SMARTCappedLogic.sol
+++ contracts/extensions/capped/internal/_SMARTCappedLogic.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: FSL-1.1-MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.28;
 
 import { _SMARTExtension } from "./../../common/_SMARTExtension.sol";
 import { ISMARTCapped } from "./../ISMARTCapped.sol";
--- test/utils/TokenUtils.sol
+++ test/utils/TokenUtils.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.28;
 
 import { Test } from "forge-std/Test.sol";
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
