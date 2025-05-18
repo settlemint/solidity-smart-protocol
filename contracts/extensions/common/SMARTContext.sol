62c6e105b13a99c108657bb7ec819e2f272bd109
Roderik van der Veer <roderik@settlemint.com>
2025-05-18 11:25:22 +0200

    chore: remove unused smartData functions

--- contracts/extensions/common/SMARTContext.sol
+++ contracts/extensions/common/SMARTContext.sol
@@ -5,6 +5,4 @@ import { Context } from "@openzeppelin/contracts/utils/Context.sol";
 
 abstract contract SMARTContext {
     function _smartSender() internal view virtual returns (address);
-
-    function _smartData() internal view virtual returns (bytes calldata);
 }
