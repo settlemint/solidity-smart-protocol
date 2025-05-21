b7e7b100a577899cf3b48f1cc102140531c87fcc
Roderik van der Veer <roderik@settlemint.com>
2025-05-21 11:06:40 +0200

    fix: update ignition code for the new code

--- ignition/modules/predeployed/system-factory.ts
+++ ignition/modules/predeployed/system-factory.ts
@@ -5,8 +5,8 @@ import IdentityModule from "./identity";
 import IdentityFactoryModule from "./identity-factory";
 import IdentityRegistryModule from "./identity-registry";
 import IdentityRegistryStorageModule from "./identity-registry-storage";
+import TokenAccessManagerModule from "./token-access-manager";
 import TrustedIssuerRegistryModule from "./trusted-issuer-registry";
-
 const SystemFactoryModule = buildModule("SystemFactoryModule", (m) => {
   const { forwarder } = m.useModule(ForwarderModule);
   const { compliance } = m.useModule(ComplianceModule);
@@ -17,6 +17,7 @@ const SystemFactoryModule = buildModule("SystemFactoryModule", (m) => {
   const { trustedIssuerRegistry } = m.useModule(TrustedIssuerRegistryModule);
   const { identityFactory } = m.useModule(IdentityFactoryModule);
   const { identity, tokenIdentity } = m.useModule(IdentityModule);
+  const { tokenAccessManager } = m.useModule(TokenAccessManagerModule);
 
   const systemFactory = m.contract("SMARTSystemFactory", [
     compliance,
@@ -26,6 +27,7 @@ const SystemFactoryModule = buildModule("SystemFactoryModule", (m) => {
     identityFactory,
     identity,
     tokenIdentity,
+    tokenAccessManager,
     forwarder,
   ]);
 
