b2cbde6ef73f8829b43df0e1c4cac61670840eca
Roderik van der Veer <roderik@settlemint.com>
2025-05-25 09:26:16 +0200

    wip

---
+++ subgraph/src/system/system.ts
@@ -0,0 +1,69 @@
+import {
+  Bootstrapped,
+  ComplianceImplementationUpdated,
+  IdentityFactoryImplementationUpdated,
+  IdentityImplementationUpdated,
+  IdentityRegistryImplementationUpdated,
+  IdentityRegistryStorageImplementationUpdated,
+  TokenAccessManagerImplementationUpdated,
+  TokenFactoryCreated,
+  TokenIdentityImplementationUpdated,
+  TrustedIssuersRegistryImplementationUpdated,
+} from "../../../generated/templates/System/System";
+import { fetchEvent } from "../event/fetch/event";
+
+export function handleBootstrapped(event: Bootstrapped): void {
+  fetchEvent(event, "Bootstrapped");
+}
+
+export function handleComplianceImplementationUpdated(
+  event: ComplianceImplementationUpdated
+): void {
+  fetchEvent(event, "ComplianceImplementationUpdated");
+}
+
+export function handleIdentityFactoryImplementationUpdated(
+  event: IdentityFactoryImplementationUpdated
+): void {
+  fetchEvent(event, "IdentityFactoryImplementationUpdated");
+}
+
+export function handleIdentityImplementationUpdated(
+  event: IdentityImplementationUpdated
+): void {
+  fetchEvent(event, "IdentityImplementationUpdated");
+}
+
+export function handleIdentityRegistryImplementationUpdated(
+  event: IdentityRegistryImplementationUpdated
+): void {
+  fetchEvent(event, "IdentityRegistryImplementationUpdated");
+}
+
+export function handleIdentityRegistryStorageImplementationUpdated(
+  event: IdentityRegistryStorageImplementationUpdated
+): void {
+  fetchEvent(event, "IdentityRegistryStorageImplementationUpdated");
+}
+
+export function handleTokenAccessManagerImplementationUpdated(
+  event: TokenAccessManagerImplementationUpdated
+): void {
+  fetchEvent(event, "TokenAccessManagerImplementationUpdated");
+}
+
+export function handleTokenFactoryCreated(event: TokenFactoryCreated): void {
+  fetchEvent(event, "TokenFactoryCreated");
+}
+
+export function handleTokenIdentityImplementationUpdated(
+  event: TokenIdentityImplementationUpdated
+): void {
+  fetchEvent(event, "TokenIdentityImplementationUpdated");
+}
+
+export function handleTrustedIssuersRegistryImplementationUpdated(
+  event: TrustedIssuersRegistryImplementationUpdated
+): void {
+  fetchEvent(event, "TrustedIssuersRegistryImplementationUpdated");
+}
