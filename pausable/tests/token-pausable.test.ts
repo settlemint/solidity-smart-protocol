2717a3b1b01a2e61a7bc64c3c2236374aef4999d
Roderik van der Veer <roderik@settlemint.com>
2025-05-26 18:42:18 +0200

    Initialize subgraph

---
+++ pausable/tests/token-pausable.test.ts
@@ -0,0 +1,48 @@
+import {
+  assert,
+  describe,
+  test,
+  clearStore,
+  beforeAll,
+  afterAll
+} from "matchstick-as/assembly/index"
+import { Address } from "@graphprotocol/graph-ts"
+import { ExampleEntity } from "../generated/schema"
+import { Paused } from "../generated/TokenPausable/TokenPausable"
+import { handlePaused } from "../src/token-pausable"
+import { createPausedEvent } from "./token-pausable-utils"
+
+// Tests structure (matchstick-as >=0.5.0)
+// https://thegraph.com/docs/en/subgraphs/developing/creating/unit-testing-framework/#tests-structure
+
+describe("Describe entity assertions", () => {
+  beforeAll(() => {
+    let sender = Address.fromString(
+      "0x0000000000000000000000000000000000000001"
+    )
+    let newPausedEvent = createPausedEvent(sender)
+    handlePaused(newPausedEvent)
+  })
+
+  afterAll(() => {
+    clearStore()
+  })
+
+  // For more test scenarios, see:
+  // https://thegraph.com/docs/en/subgraphs/developing/creating/unit-testing-framework/#write-a-unit-test
+
+  test("ExampleEntity created and stored", () => {
+    assert.entityCount("ExampleEntity", 1)
+
+    // 0xa16081f360e3847006db660bae1c6d1b2e17ec2a is the default address used in newMockEvent() function
+    assert.fieldEquals(
+      "ExampleEntity",
+      "0xa16081f360e3847006db660bae1c6d1b2e17ec2a",
+      "sender",
+      "0x0000000000000000000000000000000000000001"
+    )
+
+    // More assert options:
+    // https://thegraph.com/docs/en/subgraphs/developing/creating/unit-testing-framework/#asserts
+  })
+})
