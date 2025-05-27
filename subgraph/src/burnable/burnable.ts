1830371b0974d3aab3ffa8ad80d76a33dc15bd99
janb87 <12234016+janb87@users.noreply.github.com>
2025-05-27 14:44:40 +0200

    fix

--- subgraph/src/burnable/burnable.ts
+++ subgraph/src/burnable/burnable.ts
@@ -1,17 +1,15 @@
 import { BurnCompleted } from "../../../generated/templates/Burnable/Burnable";
 import { fetchEvent } from "../event/fetch/event";
-import { fetchTokenBalance } from "../token-balance/fetch/token-balance";
 import { decreaseTokenBalanceValue } from "../token-balance/token-balance";
 import { fetchToken } from "../token/fetch/token";
 
 export function handleBurnCompleted(event: BurnCompleted): void {
   fetchEvent(event, "BurnCompleted");
   const token = fetchToken(event.address);
-  const balance = fetchTokenBalance(token.id, event.params.from);
   decreaseTokenBalanceValue(
     token,
     event.params.from,
-    balance.valueExact.minus(event.params.amount),
+    event.params.amount,
     event.block.timestamp
   );
 }
--- subgraph/src/token-balance/token-balance.ts
+++ subgraph/src/token-balance/token-balance.ts
@@ -33,7 +33,7 @@ export function decreaseTokenBalanceValue(
   setBigNumber(
     balance,
     "value",
-    balance.valueExact.plus(value),
+    balance.valueExact.minus(value),
     token.decimals
   );
   balance.lastActivity = timestamp;
